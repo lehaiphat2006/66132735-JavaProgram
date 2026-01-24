6cf61d354d8b516a8ff44594a39fff8ad3ab433f
lehaiphat2006 <lehaiphat2006@gmail.com>
2026-01-24 19:57:18 +0700

    hinhve
    bai tap java

---
+++ hinhve/src/hinhve/hinhve.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package hinhve;
+
+/**
+ * 
+ */
+public class hinhve {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("    *   ");
+			System.out.println("   ***   ");
+			System.out.println("  *****   ");
+			System.out.println(" *******   ");
+	}
+
+}
