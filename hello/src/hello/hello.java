cdc7cb71a7b83af7135316f06dfe707d538b4c86
lehaiphat2006 <lehaiphat2006@gmail.com>
2026-01-24 19:52:53 +0700

    xinchao
    bai tap java

---
+++ hello/src/hello/hello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package hello;
+
+/**
+ * 
+ */
+public class hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chào.Tôi tên là Lê Hải Phát 66-CNTT1.");
+	}
+
+}
