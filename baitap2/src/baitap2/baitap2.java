18a73f89630e469e0447efa8a2bc1986d4b27776
lehaiphat2006 <lehaiphat2006@gmail.com>
2026-01-25 09:52:53 +0700

    baitap2
    bai tap java

---
+++ baitap2/src/baitap2/baitap2.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package baitap2;
+
+/**
+ * 
+ */
+public class baitap2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double celsius = 25.0;
+        double fahrenheit = (celsius * 9/5) + 32;
+        
+        System.out.println(celsius + "°C = " + fahrenheit + "°F");
+	}
+
+}
