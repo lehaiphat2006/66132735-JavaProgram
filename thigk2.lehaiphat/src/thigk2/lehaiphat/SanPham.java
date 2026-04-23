8a24b4e6bf31848fdbdd7a0c17fed3074de5c50f
lehaiphat2006 <lehaiphat2006@gmail.com>
2026-04-23 07:59:27 +0700

    lần 3

---
+++ thigk2.lehaiphat/src/thigk2/lehaiphat/SanPham.java
@@ -0,0 +1,67 @@
+package thigk2.lehaiphat;
+import java.util.Scanner;
+import java.util.ArrayList;
+//Tạo class SanPham (thuộc tính + constructor + getter/setter)
+//Tạo danh sách ArrayList chứa sản phẩm
+//Hard-code 3 sản phẩm ban đầu
+//In danh sách sản phẩm
+//Nhập thêm 1 sản phẩm từ bàn phím -> thêm vào danh sách
+//In lại danh sách sau khi thêm
+//Lọc và in các sản phẩm có loại = "thuc pham chuc nang"
+public class SanPham {
+	private String masp;
+	private String tensp;
+	private String loaisp;
+	private double gia;//thuộc tính thêm
+	//getter/setter
+	public String getMasp() {
+		return masp;
+	}
+
+	public void setMasp(String masp) {
+		this.masp = masp;
+	}
+
+	public String getTensp() {
+		return tensp;
+	}
+
+	public void setTensp(String tensp) {
+		this.tensp = tensp;
+	}
+
+	public String getLoaisp() {
+		return loaisp;
+	}
+
+	public void setLoaisp(String loaisp) {
+		this.loaisp = loaisp;
+	}
+
+	public double getGia() {
+		return gia;
+	}
+
+	public void setGia(double gia) {
+		this.gia = gia;
+	}
+	//constructor
+	public SanPham(String masp, String tensp, String loaisp, double gia) {
+	this.masp=masp;
+	this.tensp=tensp;
+	this.loaisp=loaisp;
+	this.gia=gia;
+	}
+	//xuất
+	public void Xuat() {
+		System.out.println("mã sp:"+masp);
+		System.out.println("tên sp:"+tensp);
+		System.out.println("loại sp:"+loaisp);
+		System.out.println("giá sp:"+gia);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
